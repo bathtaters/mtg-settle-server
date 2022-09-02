d1a400bf6a30fd0fb5348f1bf59f3de4de6e3002
bathtaters <nomoremrnice7@gmail.com>
2022-09-02 00:25:29 -0400

    Add express request types

---
+++ src/controllers/express.d.ts
@@ -0,0 +1,37 @@
+import express from 'express'
+import passport from 'passport'
+import { CleanupType } from '../services/manager.services'
+
+export declare type CleanupType = "setCards"|"cardImages"|"games"
+
+declare global {
+  namespace Express {
+    interface User {
+      username: string,
+      access: number,
+    }
+  }
+}
+
+export interface ManagerForm {
+  _action: "Update All"|"Add"|"Remove"|"Create"|"Edit"|"Delete"|"Clean",
+  updateSet?: string,
+  addSet?: string,
+  removeSet?: string,
+  newGame?: string,
+  game?: string,
+  cleanup?: CleanupType[]|CleanupType,
+  _csrf?: string,
+}
+
+export interface GameForm {
+  _action: "Replace With:"|"Cards"|"Game"|"Choose Set"|"Delete",
+  date: string,
+  position?: number|string,
+  newCard?: string,
+  newSet?: string,
+  _csrf?: string,
+}
+
+export declare type GuiHandler<Params extends object = {}> = express.RequestHandler<Params, any, {},   {}>
+export declare type FormHandler<Body  extends object = {}> = express.RequestHandler<{},     any, Body, {}>
