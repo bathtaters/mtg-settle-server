58e8d99e274013fd88832bd4266cd8d3e2a3ccb6
Nick C <bathtaters@gmail.com>
2023-09-30 00:09:28 -0400

    update csurf to replacement tiny-csrf

--- engine/middleware/csrf.middleware.js
+++ engine/middleware/csrf.middleware.js
@@ -1,4 +1,8 @@
-const csrf = require('csurf')
+const csrf = require('tiny-csrf')
+const cookies = require('cookie-parser')
 const { csrfEnable } = require('../config/server.cfg')
 
-module.exports = csrfEnable && csrf()
+const secret = (process.env.SESSION_SECRET || require('../config/settings.cfg').definitions.SESSION_SECRET.default)
+    .slice(0,32).padEnd(32,'?')
+
+module.exports = csrfEnable && [cookies(secret), csrf(secret)]
