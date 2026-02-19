4a2a1ad99ce89a1deb57075aba11583d7e7912d7
Nick C <bathtaters@gmail.com>
2026-02-19 00:39:52 -0500

    use http-proxy instead of express-http-proxy to add websockets forwarding

--- src/libs/proxy.ts
+++ src/libs/proxy.ts
@@ -1,39 +1,81 @@
-import proxy from "express-http-proxy";
+import type { RequestHandler } from "express";
+import { createProxyServer } from "http-proxy";
 import logger from "../../engine/libs/log";
 import { checkAuth } from "../../engine/middleware/auth.middleware";
+import {
+  normalizeError,
+  sendAsHTML,
+} from "../../engine/middleware/error.middleware";
 import { access } from "../../engine/config/users.cfg";
 import { gui } from "../config/urls.cfg";
+import { IncomingMessage } from "http";
+import Stream from "stream";
 
-let _enabled = false;
+let proxyIsEnabled = false;
+const url = process.env.METRICS;
 
-export default function proxyServer() {
-  const url = process.env.METRICS;
+const proxy = createProxyServer({
+  target: url,
+  changeOrigin: true,
+  ws: true,
+});
+
+export function proxyServer(): RequestHandler[] {
   if (!url) {
-    _enabled = false;
+    proxyIsEnabled = false;
     return [checkAuth(gui.root.login, access.admin)];
   }
 
-  _enabled = true;
+  proxyIsEnabled = true;
   return [
-    // Check auth/redirect
+    // Check user authentication
     checkAuth(gui.root.login, access.admin),
 
-    // Proxy to URL
-    proxy(url, {
-      proxyReqOptDecorator: (proxyReqOpts, req) => {
-        proxyReqOpts.headers["X-WEBAUTH-USER"] = req.user?.username;
-        return proxyReqOpts;
-      },
-      proxyErrorHandler: (err, _, next) => {
-        logger.error(`Proxy Error: ${err}`);
-        next({
-          status: 502,
-          name: "Proxy Error",
-          message: `Server at ${url} is currently unreachable.`,
-        });
-      },
-    }),
+    // Redirect requests to proxy
+    (req, res) => {
+      proxy.web(req, res, {
+        headers: {
+          "X-WEBAUTH-USER": req.user?.username || "",
+          Host: req.headers.host || "",
+        },
+      });
+    },
   ];
 }
 
-export const isEnabled = () => _enabled;
+/** Forward WebSockets (Server listener) */
+export const handleUpgrade = (
+  req: IncomingMessage,
+  socket: Stream.Duplex,
+  head: Buffer<ArrayBuffer>,
+) => {
+  // Handle forwarding for Grafana
+  if (
+    proxyIsEnabled &&
+    req?.url &&
+    req.url.startsWith(`${gui.admin.prefix}${gui.admin.metrics}/api/live/ws`)
+  ) {
+    proxy.ws(req, socket, head);
+  }
+};
+
+/** Global error handling for the proxy */
+proxy.on("error", (err, req, res) => {
+  logger.error(`Proxy Error: ${err}`);
+  normalizeError(
+    {
+      status: 502,
+      name: "Proxy Error",
+      message: `Via server at ${url}: ${err?.message || err || "Unknown error"}`,
+      stack: err.stack,
+    },
+    req,
+    res,
+    () => {
+      sendAsHTML(req, res);
+    },
+  );
+});
+
+/** Check if the proxy server is active */
+export const isEnabled = () => proxyIsEnabled;
--- src/server.init.ts
+++ src/server.init.ts
@@ -1,31 +1,33 @@
+import type { Server } from "http";
 import express from "express";
 import { profileMiddleware } from "../engine/libs/monitor";
 import clientRoutes from "./routes/game.routes";
 import updateRoutes from "./routes/manager.routes";
 import { pathToUrl } from "./libs/storage";
 import { gui, api } from "./config/urls.cfg";
-import proxyServer from "./libs/proxy";
+import { proxyServer, handleUpgrade } from "./libs/proxy";
 
-function startup(server: express.Application) {
-  server.locals.imageUrl = pathToUrl;
+function startup(app: express.Application, server: Server) {
+  app.locals.imageUrl = pathToUrl;
+  server.on("upgrade", handleUpgrade);
 }
 
 function teardown() {
   // Add code to execute when server begins to shutdown
 }
 
-function setup(server: express.Application) {
+function setup(app: express.Application) {
   // Add custom server settings/run-first middleware: server.set(setting, value)
 }
 
-function middleware(server: express.Application) {
-  server.use(api.prefix + api.client + "/today", profileMiddleware);
+function middleware(app: express.Application) {
+  app.use(api.prefix + api.client + "/today", profileMiddleware);
 }
 
-function routes(server: express.Application) {
-  server.use(api.prefix + api.client, clientRoutes);
-  server.use(gui.basic.prefix + gui.manage.prefix, updateRoutes);
-  server.use(gui.admin.prefix + gui.admin.metrics, proxyServer());
+function routes(app: express.Application) {
+  app.use(api.prefix + api.client, clientRoutes);
+  app.use(gui.basic.prefix + gui.manage.prefix, updateRoutes);
+  app.use(gui.admin.prefix + gui.admin.metrics, proxyServer());
 }
 
 export { setup, middleware, routes, startup, teardown };
